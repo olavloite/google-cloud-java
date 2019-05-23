7587128ecf9c5e511e3d02da4ed7f524eb035ff4
Olav Loite <koloite@gmail.com>
2019-05-23 14:49:45 +0200

    added stress test to Spanner lib

---
+++ google-cloud-clients/google-cloud-spanner/src/test/java/com/google/cloud/spanner/StressTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spanner;
+
+/**
+ * Stress Test interface. These tests are excluded from all normal test runs. To execute a specific
+ * stress test, execute <code>mvn -Pstress-test -Dtest=(test-name) test</code>.
+ */
+public interface StressTest {}
